cddc5e4e35eb3dd5021102256839dd7bf77e3dd2
danielVelasc <velasc.daniel@gmail.com>
2017-02-04 00:07:48 -0700

    Created an alarm class. Started working on saving and opening alarms

--- app/src/main/java/mobile/dp/velocityalarmclock/ClockActivity.java
+++ app/src/main/java/mobile/dp/velocityalarmclock/ClockActivity.java
@@ -8,10 +8,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
+import android.app.Fragment;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextClock;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 
 /**
@@ -24,38 +31,81 @@ import android.widget.RelativeLayout;
 public class ClockActivity extends Activity {
 
     Button addAlarmButton;
-    //Fragment addAlarmActivity;
+    Fragment addAlarmFragment; // todo make sure that the custom class is referenced here instead'
+    TextClock digitalClock;
+    Alarm[] userAlarms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clock_activity);
 
-        Log.d("CLOCK_ACTIVITY","Start-up");
+        Log.d("CLOCK_ACTIVITY","onCreate");
 
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
 
-        // get view you want to resize
-        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.current_time);
-
-        // get layout parameters for that view
+        // Get view to resize and layout parameters for that view
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.ClockLayout);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, size.y);
 
-        // initialize new parameters for my element
+        // Initialize new parameters for my element
         mainLayout.setLayoutParams(params);
 
         Log.d("CLOCK_ACTIVITY", "Height: " + size.y);
 
+        digitalClock = (TextClock)findViewById(R.id.digitalClock);
+
         addAlarmButton = (Button)findViewById(R.id.addAlarmButton);
 
-        addAlarmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO Create the view fragment
-            }
-        });
+        // Retrieve user alarms from file system
+
+    }
+
+    /**
+     * This method populates the array of alarms that have been set by a user
+     */
+    private void getAlarms() {
+
+        Log.d("CLOCK_ACTIVITY","getAlarms");
+
+        try {
+
+            FileInputStream fis = this.openFileInput(fileName);
+            ObjectInputStream is = new ObjectInputStream(fis);
+            Alarm simpleClass = (Alarm) is.readObject();
+            is.close();
+            fis.close();
+        } catch(ClassNotFoundException | IOException a) {
+            a.printStackTrace();
+            System.err.println("Error getting saved alarms");
+        }
+    }
+
+
+    void createNewAlarm() {
+        //instantiate fragment
+    }
+
+    /**
+     * Saving alarms upon application exit
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try{
+            getFilesDir();
+            FileOutputStream fos = this.openFileOutput(fileName, this.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(this);
+            os.close();
+            fos.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.err.println("Error saving alarms");
+        }
 
     }
 }
