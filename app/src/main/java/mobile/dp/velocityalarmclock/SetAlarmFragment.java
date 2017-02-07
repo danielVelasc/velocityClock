343b59e3e5e16503aadfdc8860a23c68a37a4f94
Sharon <sharon.wang16@hotmail.com>
2017-02-07 00:47:04 -0700

    Able to set an alarm for the current time

--- app/src/main/java/mobile/dp/velocityalarmclock/SetAlarmFragment.java
+++ app/src/main/java/mobile/dp/velocityalarmclock/SetAlarmFragment.java
@@ -2,14 +2,26 @@ package mobile.dp.velocityalarmclock;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
- * @author Sharon Wong
+ * @author Sharon Wang
  * @since February 4, 2017
  *
  * This class allows a user to set an alarm.
@@ -19,11 +31,52 @@ public class SetAlarmFragment extends Fragment {
 
     SetAlarmFragmentListener mListener;
     View v;
+    int day, hour, minutes;
+    Date time;
+    Button setAlarmButton;
+    TimePicker setAlarmTime;
+    Spinner daySpin; // Change to list with checkboxes (multi-selection)
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (v == null){
             v = inflater.inflate(R.layout.set_alarm_fragment, container, false);
+
+            setAlarmButton = (Button) v.findViewById(R.id.setAlarmButton);
+            setAlarmTime = (TimePicker) v.findViewById(R.id.setAlarmTime);
+            daySpin = (Spinner) v.findViewById(R.id.daySpin); // Change to list with checkboxes (multi-selection)
+
+            // Grab day, hour and minutes when setAlarmButton pressed
+            setAlarmButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    day = dayToInt(daySpin.getSelectedItem().toString());
+
+                    if (Build.VERSION.SDK_INT >= 23 ){
+                        hour = setAlarmTime.getHour();
+                        minutes = setAlarmTime.getMinute();
+                    }
+                    // Deprecated methods, but are necessary for SDK < 23
+                    else{
+                        hour = setAlarmTime.getCurrentHour();
+                        minutes = setAlarmTime.getCurrentMinute();
+                    }
+
+                    String timeString = String.valueOf(hour + minutes);
+
+                    Toast.makeText(getActivity(), "day: " + day + "\ntime: " + hour + ":" + minutes, Toast.LENGTH_SHORT).show();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+                    try {
+                        time = simpleDateFormat.parse(timeString);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    Alarm newAlarm = new Alarm(day, time, false);
+                    mListener.submitNewAlarm(newAlarm);
+                }
+            });
+
         }
         return v;
     }
@@ -39,12 +92,18 @@ public class SetAlarmFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (v == null){
-            v = inflater.inflate(R.layout.set_alarm_fragment, container, false);
-        }
-        return v;
-    }
+    public int dayToInt(String convertDay){
+        HashMap<String, Integer> convertMap = new HashMap<String, Integer>();
+            convertMap.put("Sunday",1);
+            convertMap.put("Monday",2);
+            convertMap.put("Tuesday",3);
+            convertMap.put("Wednesday",4);
+            convertMap.put("Thursday",5);
+            convertMap.put("Friday",6);
+            convertMap.put("Saturday",7);
 
+        return convertMap.get(convertDay).intValue();
+    }
 }
+
+
