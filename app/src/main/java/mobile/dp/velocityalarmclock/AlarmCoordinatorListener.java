6969c90cc654971bc380544b44a4875806824018
danielVelasc <velasc.daniel@gmail.com>
2017-02-13 13:03:52 -0700

    AlarmCoordinatorListener accidentally not pushed previously.

---
+++ app/src/main/java/mobile/dp/velocityalarmclock/AlarmCoordinatorListener.java
@@ -0,0 +1,13 @@
+package mobile.dp.velocityalarmclock;
+
+/**
+ * @author Daniel Velasco
+ * @since February 13, 2017
+ *
+ * This interface is implemented by classes that need to receive notifications
+ * every time an alarm changes (i.e. deletions, active status, modifications)
+ */
+
+public interface AlarmCoordinatorListener {
+    public void alarmChanged();
+}
