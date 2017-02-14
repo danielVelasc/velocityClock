efb7c1e5f2c6e3c83965aa769520cac79abb040c
danielVelasc <velasc.daniel@gmail.com>
2017-02-14 11:22:58 -0700

    Moved functionality to create alarm to the AlarmCoordinator. This involved modifying SetAlarmFragment (and its listener), ClockActivity

--- app/src/main/java/mobile/dp/velocityalarmclock/AlarmCoordinator.java
+++ app/src/main/java/mobile/dp/velocityalarmclock/AlarmCoordinator.java
@@ -1,5 +1,10 @@
 package mobile.dp.velocityalarmclock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
 import java.util.ArrayList;
 
 /**
@@ -18,8 +23,9 @@ import java.util.ArrayList;
  * register method and implementing the corresponding interface.
  */
 
-public class AlarmCoordinator {
+class AlarmCoordinator {
 
+    private Context context;
     private ArrayList<Alarm> alarmList;
     private ArrayList<AlarmCoordinatorListener> listeners;
 
@@ -33,15 +39,32 @@ public class AlarmCoordinator {
         }
     }
 
-    protected AlarmCoordinator getInstance ()
+    protected static AlarmCoordinator getInstance ()
     { return instance; }
 
-    void createNewAlarm(Alarm newAlarm) {
+    void createNewAlarm(Alarm alarm) {
 
+        Intent alertIntent = new Intent(context, AlarmReceiver.class); //When timer ends, check with receiver
+        alertIntent.putExtra("Alarm-Name", alarm.getName());
+        alertIntent.putExtra("Alarm-ID", alarm.getUuid()); // Will be helpful later
+
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if (alarm.repeats()) //Schedule alarm to repeat if necessary
+            alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, alarm.getTimeToGoOff(), 24 * 60 * 60 * 1000, PendingIntent.getBroadcast(context, 1, alertIntent, PendingIntent.FLAG_UPDATE_CURRENT)); //Repeats every 24 hours after
+        else
+            alarmMgr.set(AlarmManager.RTC_WAKEUP, alarm.getTimeToGoOff(), PendingIntent.getBroadcast(context, 1, alertIntent, PendingIntent.FLAG_ONE_SHOT));
+
+        alarm.setState(true);
+        alarmList.add(alarm);
         notifyAlarmChange();
     }
 
     void deleteAlarm(Alarm alarm) {
+
+
+
+
 
         notifyAlarmChange();
     }
@@ -60,4 +83,7 @@ public class AlarmCoordinator {
         listeners.add(listener);
     }
 
+    void setContext(Context context) {
+        this.context = context;
+    }
 }
