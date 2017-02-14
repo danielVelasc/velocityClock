061f24c1686fdd5cda4b3217f2c4ae6d657d5e9f
danielVelasc <velasc.daniel@gmail.com>
2017-02-14 11:36:42 -0700

    Bugs with the AlarmCoordinator were solved!

--- app/src/main/java/mobile/dp/velocityalarmclock/AlarmCoordinator.java
+++ app/src/main/java/mobile/dp/velocityalarmclock/AlarmCoordinator.java
@@ -30,26 +30,23 @@ class AlarmCoordinator {
     private ArrayList<Alarm> alarmList;
     private ArrayList<AlarmCoordinatorListener> listeners;
 
-    private static AlarmCoordinator instance = null;
+    private static AlarmCoordinator instance = new AlarmCoordinator();
 
     private AlarmCoordinator () {
-        if(instance == null) {
-            alarmList = new ArrayList<>();
-            listeners = new ArrayList<>();
-            instance = new AlarmCoordinator();
+        alarmList = new ArrayList<>();
+        listeners = new ArrayList<>();
 
-            //TODO: Deserialize the alarms using function
-            //getAlarms();
+        //TODO: Deserialize the alarms using function
+        //getAlarms();
 
-            // Test alarms
-            alarmList.add(new Alarm(1, new Date(1000000), true));
-            alarmList.add(new Alarm(2, new Date(2000000), false));
-            alarmList.add(new Alarm(3, new Date(3000000), true));
-            alarmList.add(new Alarm(4, new Date(4000000), true));
+        // Test alarms
+        alarmList.add(new Alarm(1, new Date(1000000), true));
+        alarmList.add(new Alarm(2, new Date(2000000), false));
+        alarmList.add(new Alarm(3, new Date(3000000), true));
+        alarmList.add(new Alarm(4, new Date(4000000), true));
 
-            //TODO: Fix issue where one less alarm will be displayed
-            // alarmList.add(null);
-        }
+        //TODO: Fix issue where one less alarm will be displayed
+        // alarmList.add(null);
     }
 
     protected static AlarmCoordinator getInstance ()
